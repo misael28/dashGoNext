1765adcecb40dffcecbeb370cb50b5e347d8e5fe
misaelpaperstack <misael@paperstack.us>
2021-10-15 22:58:46 -0300

    feat: create dashboard icons side menu

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import { Box, Stack, Text, Link, Icon } from "@chakra-ui/react";
+import { RiDashboardLine, RiContactsLine } from "react-icons/ri";
+export function Sidebar() {
+  return (
+    <Box as="aside" w="64" mr="8">
+      <Stack spacing="12" align="flex-start">
+        <Box>
+          <Text fontWeight="bold" color="gray.400" fontSize="small">
+            GERAL
+          </Text>
+          <Stack spacing="4" mt="8" align="stretch">
+            <Link display="flex" align="strech">
+              <Icon as={RiDashboardLine} fontSize="20" />
+              <Text ml="4" fontWeight="medium">
+                Dashboard
+              </Text>
+            </Link>
+            <Link display="flex" align="strech">
+              <Icon as={RiContactsLine} fontSize="20" />
+              <Text ml="4" fontWeight="medium">
+                Usuários
+              </Text>
+            </Link>
+          </Stack>
+        </Box>
+      </Stack>
+    </Box>
+  );
+}
