7fcfbc3c2a36113a09f19f498ba91c51e3405dab
misael <misael.geof@gmail.com>
2021-11-27 14:27:07 -0300

    add create user

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -6,6 +6,7 @@ import {
     Td, Checkbox, Text  } from '@chakra-ui/react';
 import { RiAddLine, RiEditLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
+import Pagination from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList(){
@@ -65,6 +66,7 @@ export default function UserList(){
                                 </Tr>
                             </Tbody>
                             </Table>  
+                            <Pagination />
                 </Box>
 
             </Flex>
