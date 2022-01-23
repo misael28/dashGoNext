879adb274eaf288b0700fb7c17ee46d0df8e5021
misael <misael.geof@gmail.com>
2022-01-23 17:07:07 -0300

    add mirage functions

---
+++ src/services/mirage/index.ts
@@ -0,0 +1,23 @@
+import { createServer, Model } from "miragejs";
+
+type User = {
+    name: string;
+    email: string;
+    created_at: string;
+};
+
+export function makeServer(){
+    const server = createServer({
+        models: {
+            users: Model.extend<Partial<User>>({}),
+        },
+        
+        routes(){
+            this.namespace = "api";
+            this.get("/users");
+            this.post("/users");
+        }
+    })
+
+    return server;
+};
